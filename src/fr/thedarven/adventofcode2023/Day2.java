dec2daa4be9b0bb2f59947a87ec52849dc2f3cf8
Lucas Geoffroy <lucas.geoffroy@indy.fr>
2023-12-04 09:51:22 +0100

    Day 4

--- src/fr/thedarven/adventofcode2023/Day2.java
+++ src/fr/thedarven/adventofcode2023/Day2.java
@@ -1,8 +1,6 @@
 package fr.thedarven.adventofcode2023;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day2 {
 
@@ -13,39 +11,27 @@ public class Day2 {
     public static void part1() {
         Scanner sc = new Scanner(System.in);
 
-        Map<String, Integer> filters = new HashMap<>();
-        filters.put("red", 12);
-        filters.put("green", 13);
-        filters.put("blue", 14);
-
         String line;
-        String[] sets, elements, value;
         int result = 0;
-        int gameNumber = 1;
         while (true) {
-            boolean isGamePossible = true;
             line = sc.nextLine();
-            if (line.isEmpty()) {
+            if (line.isEmpty() || line.isBlank()) {
                 break;
             }
-            sets = line.split(": ")[1].split("; ");
-            for (String set: sets) {
-                elements = set.split(", ");
-                for (String element: elements) {
-                    value = element.split(" ");
-                    if (filters.get(value[1]) < Integer.parseInt(value[0])) {
-                        isGamePossible = false;
-                        break;
-                    }
-                }
-                if (!isGamePossible) {
-                    break;
+            String[] winningNumbersS = line.split(": ")[1].split(" \\| ")[0].split("\\s+");
+            String[] numbersS = line.split(": ")[1].split(" \\| ")[1].split("\\s+");
+            List<Integer> winningNumbers = Arrays.stream(winningNumbersS).filter(s -> !s.isBlank()).map(Integer::parseInt).toList();
+            List<Integer> numbers = Arrays.stream(numbersS).filter(s -> !s.isBlank()).map(Integer::parseInt).toList();
+
+            int nbElement = 0;
+            for (int num: numbers) {
+                if (winningNumbers.contains(num)) {
+                    nbElement++;
                 }
             }
-            if (isGamePossible) {
-                result += gameNumber;
+            if (nbElement > 0) {
+                result += (int) Math.pow(2, nbElement - 1);
             }
-            gameNumber++;
         }
         System.out.println(result);
     }
@@ -54,33 +40,42 @@ public class Day2 {
         Scanner sc = new Scanner(System.in);
 
         String line;
-        String[] sets, elements, value;
-        int result = 0, nbRed, nbGreen, nbBlue;
+        int result = 0;
+        List<Integer> amount = new ArrayList<>();
+        amount.add(1);
+        int cardId = 0;
         while (true) {
-            nbRed = 0; nbGreen = 0; nbBlue = 0;
             line = sc.nextLine();
-            if (line.isEmpty()) {
+            if (line.isEmpty() || line.isBlank()) {
                 break;
             }
-            sets = line.split(": ")[1].split("; ");
-            for (String set: sets) {
-                elements = set.split(", ");
-                for (String element: elements) {
-                    value = element.split(" ");
-                    switch (value[1]) {
-                        case "red":
-                            nbRed = Math.max(nbRed, Integer.parseInt(value[0]));
-                            break;
-                        case "green":
-                            nbGreen = Math.max(nbGreen, Integer.parseInt(value[0]));
-                            break;
-                        case "blue":
-                            nbBlue = Math.max(nbBlue, Integer.parseInt(value[0]));
-                            break;
-                    }
+
+            if (amount.size() == cardId) {
+                amount.add(1);
+            }
+
+            result += amount.get(cardId);
+
+            String[] winningNumbersS = line.split(": ")[1].split(" \\| ")[0].split("\\s+");
+            String[] numbersS = line.split(": ")[1].split(" \\| ")[1].split("\\s+");
+            List<Integer> winningNumbers = Arrays.stream(winningNumbersS).filter(s -> !s.isBlank()).map(Integer::parseInt).toList();
+            List<Integer> numbers = Arrays.stream(numbersS).filter(s -> !s.isBlank()).map(Integer::parseInt).toList();
+
+            int nbElement = 0;
+            for (int num: numbers) {
+                if (winningNumbers.contains(num)) {
+                    nbElement++;
                 }
             }
-            result += nbRed * nbGreen * nbBlue;
+            for (int i = cardId + 1; i < cardId + nbElement + 1; i++) {
+                if (amount.size() > i) {
+                    amount.set(i, amount.get(i) + amount.get(cardId));
+                } else {
+                    amount.add(amount.get(cardId) + 1);
+                }
+            }
+
+            cardId++;
         }
         System.out.println(result);
     }
